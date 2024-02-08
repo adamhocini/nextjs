56e3e4f522a4a417323aa6f7778c7733a388b2cd
Adam Hocini <adam.hocini.ah@icloud.com>
2024-02-08 13:40:25 +0100

    dashboard

--- project/my-app/app/ui/dashboard/card/card.tsx
+++ project/my-app/app/ui/dashboard/card/card.tsx
@@ -1,10 +1,26 @@
 import { MdSupervisedUserCircle } from 'react-icons/md'
 import styles from './card.module.css'
 
-const Card = () => {
+interface CardProps {
+  title: string;
+  number: string;
+  detail: string;
+  isPositive?: boolean;
+}
+
+const Card: React.FC<CardProps> = ({ title, number, detail, isPositive = true }) => {
+  const numberClassName = isPositive ? styles.positive : styles.negative;
+
     return (
       <div className={styles.container}>
         <MdSupervisedUserCircle size={24}/>
+        <div className={styles.texts}>
+          <span className={styles.title}>Total Users</span>
+          <span className={styles.number}>10.273</span>
+          <span className={styles.detail}>
+            <span className={styles.positive}>12%</span> more than previous week
+          </span>
+        </div>
       </div>
     )
   }
